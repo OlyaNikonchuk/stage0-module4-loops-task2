5658978641c64b9c916a75239d63608a8cf6a7cc
OlyaNikonchuk <olya.nikonchuk02@gmail.com>
2022-07-31 16:28:40 +0300

    first attempt

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int count=0;
+        int temp=0;
+        if (multiplyByAndToInclusive < 0) {
+            temp = -1 * multiplyByAndToInclusive;
+        } else {
+            temp = multiplyByAndToInclusive;
+        }
+        if(multiplyByAndToInclusive!=0) {
+            while (count <= temp) {
+                System.out.println(count*multiplyByAndToInclusive);
+                count++;
+            }
+        }
+        }
     }
-}
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int count = 1;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            int result = 1;
+            System.out.println(1);
+            while (count <= power) {
+                result = result * 2;
+                System.out.println(result);
+                count++;
+            }
+        }
     }
 }
